1c66c09b2da588f6ecc43bd846bd4fda9a6e7407
Yogita Subhedar <Yogita Subhedar@DESKTOP-RTM7CBO>
2017-12-20 01:39:47 +0530

    git testing

---
+++ MavenDemo/src/test/java/com/maven/test/NewTest.java
@@ -0,0 +1,10 @@
+package com.maven.test;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f1() {
+	  System.out.println("Welcome to Selenium World!!!");
+  }
+}
---
+++ MavenDemo/src/test/java/com/maven/test/NewTest2.java
@@ -0,0 +1,10 @@
+package com.maven.test;
+
+import org.testng.annotations.Test;
+
+public class NewTest2 {
+  @Test
+  public void f2() {
+	  System.out.println("Welcome to Selenium World!!!");
+  }
+}
